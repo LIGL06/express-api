5d6d0f19384f721ebd476d2a64cc20d1c0f83d9b
Ivan Garcia <luis.garcialuna@outlook.com>
2021-02-01 11:36:16 -0600

    Add Session Validation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const env = require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const jwt = require('express-jwt');
 // routers
 const usersRouter = require('./routes/users');
 
@@ -13,6 +14,24 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(jwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ['HS256'],
+  credentialsRequired: true,
+  getToken: function fromHeaderOrQuerystring (req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    }
+    return null;
+  }
+}).unless({ path: ['/session'] }));
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Invalid Token');
+  }
+});
 // routes
 app.use('/users', usersRouter);
 
